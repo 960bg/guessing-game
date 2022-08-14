40b86ae5738fa1df448ec1fb263da5a6e25b0a3c
@960bg Mihail Dementev <-h>
2022-08-14 15:34:47 +0400

    feat: task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,37 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        let arr = [];
+        for (let i = min; i <= max; i++) {
+            arr.push(i);
+        }
+        this.result = arr[Math.round((arr.length - 1) / 2)];
+        return this.result;
     }
 
     guess() {
+        return this.result;
 
     }
 
     lower() {
-
+        this.setRange(this.min, this.result);
     }
 
     greater() {
-
+        this.setRange(this.result, this.max);
     }
 }
 
 module.exports = GuessingGame;
+
+// let game = new GuessingGame();
+
+// console.log(game.setRange(0, 15));
